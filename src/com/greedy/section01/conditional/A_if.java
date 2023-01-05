2b0e4cc3924a8483892245af92c8c7f8cb6d18e9
tjoeun <tjoeun@DESKTOP-J5A7LB8>
2023-01-05 14:55:38 +0900

    conditional

---
+++ src/com/greedy/section01/conditional/A_if.java
@@ -0,0 +1,11 @@
+package com.greedy.section01.conditional;
+
+public class A_if {
+	
+	public void testSimpleIfStatement() {
+		
+		/* 
+		 * */
+		
+	}
+}
---
+++ src/com/greedy/section01/conditional/Application.java
@@ -0,0 +1,10 @@
+package com.greedy.section01.conditional;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
